8ce58dc26bba179dc9997714a21a9dea43061a13
veenashivpooji <veenashivpooji@gmail.com>
2026-07-30 13:09:23 +0530

    AddedNewStatement

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,6 +5,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to the Java Track");
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using Java");
-
+        System.out.println("Learning Git is fun");
     }
 }
