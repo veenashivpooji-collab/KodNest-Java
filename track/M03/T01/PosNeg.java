76ad0f46285b7f3caed9675b7987af02eb4ee1af
veenashivpooji <veenashivpooji@gmail.com>
2026-08-04 12:36:10 +0530

    PosNeg

---
+++ track/M03/T01/PosNeg.java
@@ -0,0 +1,15 @@
+
+public class PosNeg {
+
+    public static void main(String[] args) {
+        int num = 5;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("0");
+        }
+
+    }
+}
