396ca15f9bf932e23030d8d4a0832bc722897cf4
veenashivpooji <veenashivpooji@gmail.com>
2026-08-05 11:21:38 +0530

    Pgm1

---
+++ track/M03/T01/Placement.java
@@ -0,0 +1,19 @@
+
+public class Placement {
+
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible =  >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean 
+
+
+
+
+            
+    }
+}
+}
