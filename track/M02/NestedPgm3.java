507ad0adc730cf27ba12485e285459308c7bb7f5
veenashivpooji <veenashivpooji@gmail.com>
2026-08-06 13:01:49 +0530

    NestedPgm3

---
+++ track/M02/NestedPgm3.java
@@ -0,0 +1,16 @@
+
+public class NestedPgm3 {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.err.println("i: " + i + " j: " + j);
+            }
+        }
+    }
+}
