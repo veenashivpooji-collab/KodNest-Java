17c4b6179a7b32289127a4a4cca92f8c2e55e3d1
veenashivpooji <veenashivpooji@gmail.com>
2026-08-11 11:12:37 +0530

    EmpApplication

---
+++ track/M02/Employee.java
@@ -0,0 +1,34 @@
+
+class EmpApplication {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.id = 11;
+        e1.name = "Anu";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+
+        e1.work();
+
+        Employee e2;
+        e2 = e1;
+        e2.id = 12;
+        e2.name = "Raja";
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+
+    }
+}
+
+class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("working");
+    }
+}
