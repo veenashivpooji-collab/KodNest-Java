c2ad4c00a88f0acdf95a8f0d68bb4ca22aff685f
veenashivpooji <veenashivpooji@gmail.com>
2026-09-08 17:44:28 +0530

    StringMethod2

---
+++ track/M4/Strings/StringMethod2.java
@@ -0,0 +1,10 @@
+
+public class StringMethod2 {
+
+    public static void main(String[] args) {
+        String s1 = "    Java    ";
+        System.out.println(s1);
+        System.out.println(s1.trim());
+
+    }
+}
