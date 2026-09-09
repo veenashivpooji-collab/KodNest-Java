69f6cb6c4fd809844ce1d8462ee795b2b58ff507
veenashivpooji <veenashivpooji@gmail.com>
2026-09-09 16:47:20 +0530

    StringComparision

---
+++ track/M4/Strings/StringComparision.java
@@ -0,0 +1,10 @@
+
+public class StringComparision {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+        System.out.println(res);
+        System.out.println(s1);
+    }
+}
